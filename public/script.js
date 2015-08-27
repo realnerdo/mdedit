1e8245c88cc87f17170ecec5acbbddb525c3ad8b
Asael Chavez <achavez@gammapartners.com>
2015-08-27 14:38:21 -0600

    Added realtime and multiple docs functionality

--- public/script.js
+++ public/script.js
@@ -3,13 +3,42 @@ window.onload = function(){
         pad = document.getElementById('pad'),
         markdownArea = document.getElementById('markdown');
 
+    var previousMarkdownValue;
+
     var convertedTextAreaToMarkdown = function(){
             var markdownText = pad.value;
             html = converter.makeHtml(markdownText);
             markdownArea.innerHTML = html;
     };
 
+    var didChangeOccur = function(){
+            if(previousMarkdownValue != pad.value){
+                return true;
+            }
+            return false;
+    };
+
+    setInterval(function(){
+        if(didChangeOccur()){
+            convertedTextAreaToMarkdown();
+        }
+    }, 1000);
+
     pad.addEventListener('input', convertedTextAreaToMarkdown);
+
+    // convertedTextAreaToMarkdown();
+
+    sharejs.open(document.location.pathname, 'text', function(error, doc){
+        doc.attach_textarea(pad);
+    });
+
+    if(document.location.pathname.length > 1){
+        var documentName = document.location.pathname.substring(1);
+        sharejs.open(documentName, 'text', function(error, doc){
+            doc.attach_textarea(pad);
+            convertedTextAreaToMarkdown();
+        });
+    }
 
     convertedTextAreaToMarkdown();
 };
--- server.js
+++ server.js
@@ -9,5 +9,19 @@ app.get('/', function(req, res){
     res.render('pad');
 });
 
+app.get('/(:id)', function(req, res){
+    res.render('pad');
+});
+
+var sharejs = require('share');
+
+require('redis');
+
+var options = {
+    db: {type:'redis'},
+};
+
+sharejs.server.attach(app, options);
+
 var port = process.env.port || 8000;
 app.listen(port);
